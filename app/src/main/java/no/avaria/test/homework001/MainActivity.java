77510b1e845a361247b3dfb605648a4770b5a93c
FCKate <78856443+FCKate@users.noreply.github.com>
2021-04-13 09:49:41 +0300

    Init project

---
+++ app/src/main/java/no/avaria/test/homework001/MainActivity.java
@@ -0,0 +1,18 @@
+package no.avaria.test.homework001;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Notification;
+import android.graphics.LightingColorFilter;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page_two);
+
+    }
+}
